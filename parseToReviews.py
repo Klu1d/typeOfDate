d0a67b0db0ac1672f11a728d413f8af3b82aa906
Ислам Асабаев <105982046+Klu1d@users.noreply.github.com>
2023-05-02 17:00:49 +0300

    Update parseToReviews.py

--- parseToReviews.py
+++ parseToReviews.py
@@ -1,18 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-url1 = "https://otziv-otziv.ru/katalog/limonady-i-gazirovannye-napitki/coca-cola/gazirovannyy-napitok-coca-cola-classic.html?page=60"
-url2 = "https://otziv-otziv.ru/katalog/limonady-i-gazirovannye-napitki/coca-cola/gazirovannyy-napitok-coca-cola-classic.html"
+url = "https://otziv-otziv.ru/katalog/limonady-i-gazirovannye-napitki/coca-cola/gazirovannyy-napitok-coca-cola-classic.html?page=60"
+
 
 """Парсинг всех отзывов, с 60 страницы """
-response1 = requests.get(url1)
+response1 = requests.get(url)
 soup1 = BeautifulSoup(response1.content, "lxml")
 reviews = soup1.find_all('div', 'container-reviews collapsible collapsed')
 for review in reviews:
     print(review.get_text(strip=True, separator=' '))
 
 """Получить название товаров аналогов газированной воды Чупа-Чупса"""
-response2 = requests.get(url2)
+response2 = requests.get(url)
 soup2 = BeautifulSoup(response2.content, "lxml")
 names = soup2.find_all('h6')
 print("\nТовары аналоги Газированный напиток Coca-Cola Classic:\n")
